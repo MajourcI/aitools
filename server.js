fdd9ee076a4fcff1394591427e071cab732da1dd
MajourcI <gejko02@gmail.com>
2026-06-01 01:09:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '8mb' }));
 app.use(express.static('public'));
 
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
@@ -216,4 +216,5 @@ ${userText}`,
 
 const PORT = process.env.PORT || 3000;
 require('./routes_study')(app, callGroqChat);
+require('./routes_ocr')(app);
 app.listen(PORT, () => console.log(`Сервер на порту ${PORT}`));
